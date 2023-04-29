cbcf822f9c150dc70646f402a204ea8d972d6409
nicholas489 <106541221+nicholas489@users.noreply.github.com>
2023-04-29 13:36:56 -0400

    Update Button.java

--- Button.java
+++ Button.java
@@ -1,14 +1,12 @@
 import javax.swing.JButton;
-
 public class Button extends JButton {
     private static int numOfButtons = 0;
     private final String name;
     public Button() {
-        setBounds(numOfButtons * 140 + 40, 250, 100, 100);
-        numOfButtons++;
-        this.name = "Button " + numOfButtons;
+        setBounds(numOfButtons * 140 + 40, 250, 100, 100); // Sets the orientation of this object in the GUI
+        numOfButtons++; // Keeps track of how many Buttons are instantiated
+        this.name = "Button " + numOfButtons; // Sets the name of this Button to "Button " + numOfButtons
     }
     @Override
-    public String toString() {return name;}
-
+    public String toString() {return name;} // Overrides toString() to return the name of this button
 }
